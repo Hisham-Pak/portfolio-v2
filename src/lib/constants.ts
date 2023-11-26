15f278036431aee0630b55ecbbf1815d1c69dbfa
Hisham Mahmood <45965466+Hisham-Pak@users.noreply.github.com>
2023-11-26 11:47:21 +0000

    Improve seo and accessibility

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,5 @@
 export const LINKS = {
   github: "https://github.com/Hisham-Pak/",
   linkedin: "https://www.linkedin.com/in/hishammahmood/",
+  stackoverflow: "https://stackoverflow.com/users/11594778/hisham-pak",
 };
